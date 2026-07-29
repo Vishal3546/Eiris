7f17710c9ce032b2e148789c6469790cc1342aec
kaushalp8095 <kaushalp8095@gmail.com>
2026-07-29 15:37:33 +0530

    backend

--- backend/src/main/java/com/eiris/backend/config/CorsConfig.java
+++ backend/src/main/java/com/eiris/backend/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.eiris.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,10 +14,13 @@ import java.util.List;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    private List<String> allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:3000")); // Restrict origins when allowCredentials is true
+        configuration.setAllowedOrigins(allowedOrigins); // Restrict origins when allowCredentials is true
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
         configuration.setExposedHeaders(List.of("Authorization"));
